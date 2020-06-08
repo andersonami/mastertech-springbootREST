a3093d1e69a538942369a73391965566328cc5c1
andersonami <andersonamisp@gmail.com>
2020-06-07 22:18:31 -0300

    adequando o servico para contemplar os metodos de persistencia

--- src/main/java/com/itau/SpringBootREST/service/ServiceInvestimento.java
+++ src/main/java/com/itau/SpringBootREST/service/ServiceInvestimento.java
@@ -1,9 +1,24 @@
 package com.itau.SpringBootREST.service;
 
+import com.itau.SpringBootREST.persistence.Investimento;
+import com.itau.SpringBootREST.persistence.InvestimentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ServiceInvestimento {
+    @Autowired
+    private InvestimentoRepository investimentoRepository;
+
+    public List<Investimento> getAll(){
+        return investimentoRepository.findAll();
+    }
+    public Investimento create(Investimento investimento){
+        return investimentoRepository.save(investimento);
+    }
+
+
 
     public double simular(Investimento dadosentrada) {
         //prazo em meses
