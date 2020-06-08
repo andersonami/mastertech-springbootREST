cdd1a81b2559b052688420ab2b9c5659b0a765f7
andersonami <andersonamisp@gmail.com>
2020-06-07 22:22:13 -0300

    adequando o controller para persistir

--- src/main/java/com/itau/SpringBootREST/web/ControllerInvestimento.java
+++ src/main/java/com/itau/SpringBootREST/web/ControllerInvestimento.java
@@ -1,6 +1,6 @@
 package com.itau.SpringBootREST.web;
 
-import com.itau.SpringBootREST.service.Investimento;
+import com.itau.SpringBootREST.persistence.Investimento;
 import com.itau.SpringBootREST.service.ServiceInvestimento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +12,20 @@ public class ControllerInvestimento {
     @Autowired
     private ServiceInvestimento serviceInvestimento;
 
-    @GetMapping("/Test")
-    public String teste(){
-        return "Hello World ! Im alive ! " ;
+    public List<Investimento> getAll(){
+        return serviceInvestimento.getAll();
+    }
+
+
+    @GetMapping("/about")
+    public String about(){
+        return "Hello World ! Im alive ! Version 0.0.2" ;
     }
 
     @PostMapping("/Investimento")
     public Investimento simularInvestimento(@RequestBody Investimento dadosEntrada){
         dadosEntrada.setValorFinal(serviceInvestimento.simular(dadosEntrada));
-        return dadosEntrada;
+        return serviceInvestimento.create(dadosEntrada);
     }
 
 }
